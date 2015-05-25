f7ad4960953a4767b0c3ee764cde24d837dbed5f
Vincent de Belleval <2000db@gmail.com>
2015-05-25 21:10:32 +0100

    always check for links on load().  zoom mouseover

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,32 @@
+function createLinks()
+{
+    //Entry links
+    $(".projectEntry a").click(function() {
+        var href = $(this).attr("href");
+        history.pushState({}, '', href);
+        //$main.hide();
+        $main.load(href + ' #project');
+        //$main.fadeIn();
+        return false;
+    });
+
+}
+
+function addZoom()
+{
+    $("article img").hover(function() {
+        $(this).css( 'cursor', 'url(/media/zoom.png), auto' );
+        $(this).click(function() {
+            alert('zoom it');
+        });
+    });
+
+}
+
+
 (function ($) {
     "use strict";
     $(document).ready(function(){
-
         // Hide / Show menu on scroll
         $("#sidebar" ).scroll(function() {
             var scrollPos = $("#sidebar").scrollTop();
@@ -15,32 +40,48 @@
             var $posts = $("#postItems"),
 
             init = function() {
-                //var href = $("latest").attr("href");
-                //$main.load(href + ' #project');
+                var href = $("latest").attr("href");
+                $main.load(href + ' #project');
                                 
             };
+
+
+            createLinks = function() {
+                //Entry links
+                $(".projectEntry a").click(function() {
+                    var href = $(this).attr("href");
+                    history.pushState({}, '', href);
+                    //$main.hide();
+                    $main.load(href + ' #project');
+                    //$main.fadeIn();
+                    return false;
+                });
+            };
+
+
             init();
+            createLinks();
+            addZoom();
 
-            // Entry links
-            // $(".projectEntry a").click(function() {
-            //     var href = $(this).attr("href");
-            //     history.pushState({}, '', href);
-            //     $main.load(href + ' #project');
-            //     return false;
-            // });
+            // Menu links
+            $("#buttons a").click(function() {
+                var href = $(this).attr("href");
+                history.pushState({}, '', href);
+                if(href.indexOf("/info") >= 0) {
+                    $main.load(href + ' #project', function() {
+                        createLinks();
+                    });
+                } else {
+                    $posts.load(href + ' #menu', function() {
+                        createLinks();
+                    });
+                }
+                
+                return false;
+            });
+            
 
-            // // Menu links
-            // $("#buttons a").click(function() {
-            //     var href = $(this).attr("href");
-            //     history.pushState({}, '', href);
-            //     if(href.indexOf("/info") >= 0) {
-            //         $main.load(href + ' #project');
-            //     } else {
-            //         $posts.load(href + ' #menu');
-            //     }
-            //     return false;
-            // });
-
+ 
         });
         
     });
